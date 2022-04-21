881052e760c1a09d3983570c8a71a8bbae8faba0
DhruvilPatel <atdhruvilpatel@gmail.com>
2022-04-21 00:21:53 -0400

    updated app scr

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -20,10 +20,6 @@ class _LandingPageState extends State<LandingPage> {
     if (!await launch(GITHUB_URL)) throw 'Could not launch $GITHUB_URL';
   }
 
-  void _launchProxyServerPage() async {
-    if (!await launch(PROXY_SERVER)) throw 'Could not launch $PROXY_SERVER';
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,44 +62,6 @@ class _LandingPageState extends State<LandingPage> {
                 ),
               ),
               const SizedBox(height: 45.0),
-              SizedBox(
-                width: 600.0,
-                child: SelectableText.rich(
-                  TextSpan(
-                    style: const TextStyle(color: NodblixStyles.primaryColor),
-                    text: 'Go to ',
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: 'https://cors-anywhere.herokuapp.com/corsdemo',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            backgroundColor: NodblixStyles.primaryColor,
-                            color: Colors.grey[900],
-                          ),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              _launchProxyServerPage();
-                            }),
-                      const TextSpan(
-                        text: ' and click ',
-                      ),
-                      const TextSpan(
-                        text: ' Request temporary access to the demo server ',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const TextSpan(
-                        text:
-                            ' button to make this app work. Now, click `Explore Nodblix` to view data in the playground.',
-                      ),
-                    ],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              const SizedBox(height: 25.0),
               ElevatedButton.icon(
                 onPressed: () async {
                   await Navigator.of(context).pushNamed('/playground');
